d9bd23fd6550414e200d61a1011cad8b93730360
jolenezheng <jolenezheng18@gmail.com>
2021-02-11 11:53:59 -0500

    finished session model

--- enrolments/admin.py
+++ enrolments/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Session
+
 # Register your models here.
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "season",
+        "year",
+    )
+    ordering = (
+        "id",
+        "year",
+    )
+    search_fields = (
+        "season",
+        "year",
+    )
+    list_filter = ("season","year",)
+
+
+admin.site.register(Session, SessionAdmin)
--- enrolments/models.py
+++ enrolments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Session(models.Model):
+  SPRING = "Spring"
+  SUMMER = "Summer"
+  FALL = "Fall"
+  SEASON_CHOICES = [
+    (SPRING, "Spring"),
+    (SUMMER, "Summer"),
+    (FALL, "Fall"),
+  ]
+
+  season = models.CharField(max_length=6, choices=SEASON_CHOICES)
+  year = models.PositiveSmallIntegerField()
+
+  def __str__(self):
+      return self.season
