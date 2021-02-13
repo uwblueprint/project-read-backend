c40b4e3057d34da4ba43e42d9999052bd5ea39dd
Tayef Shah <tayefshah@gmail.com>
2021-02-13 15:26:31 -0500

    added class model to enrolments app

--- enrolments/models.py
+++ enrolments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Class(models.Class):
+    # by default, id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    # session = models.ForeignKey()
+    # facilitator = models.ForeignKey()
+
+def __str__(self):
+    return self.name
