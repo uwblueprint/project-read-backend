1948440572c6b7f624e4e174dee38ab8ca67b2db
Tayef Shah <tayefshah@gmail.com>
2021-02-13 16:33:58 -0500

    cleaned comments

--- enrolments/models.py
+++ enrolments/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Class(models.Model):
-    # by default, id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128)
     # session = models.ForeignKey()
     # facilitator = models.ForeignKey()
