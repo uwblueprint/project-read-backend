11bb9f153bf6c8366c0306f96644418c7757a46c
Daniel <daniel.chen115@gmail.com>
2021-02-13 11:27:53 -0500

    Use mixins, permrissions

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import UserViewSet
+from .views import UserViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
--- accounts/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import User
-from .serializers import UserSerializer
-
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
