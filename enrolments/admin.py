1c9d4af97377929294544ecba8219ab7b0ef458f
Tayef Shah <tayefshah@gmail.com>
2021-02-13 15:49:16 -0500

    linted files, and fixed type on class model

--- enrolments/admin.py
+++ enrolments/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 
 from .models import Class
 
+
 class ClassAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "name",
     )
-    ordering = ("-id")
-    search_fields = (
-        "name"
-    )
+    ordering = "-id"
+    search_fields = "name"
+
 
 admin.site.register(Family, FamilyAdmin)
--- enrolments/models.py
+++ enrolments/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-class Class(models.Class):
+
+class Class(models.Model):
     # by default, id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128)
     # session = models.ForeignKey()
     # facilitator = models.ForeignKey()
+
 
 def __str__(self):
     return self.name
