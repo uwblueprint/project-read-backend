d74446c2060ef0fc2607b31b82aa90df1f4e32a2
jolenezheng <jolenezheng18@gmail.com>
2021-02-16 20:36:09 -0500

    resolved merge conflicts

--- enrolments/admin.py
+++ enrolments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Class
 from .models import Session
 
 # Register your models here.
@@ -26,3 +26,15 @@ class SessionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Session, SessionAdmin)
+
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    ordering = ("-id",)
+    search_fields = ("name",)
+
+
+admin.site.register(Class, ClassAdmin)
--- enrolments/models.py
+++ enrolments/models.py
@@ -18,3 +18,12 @@ class Session(models.Model):
 
     def __str__(self):
         return self.season
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=128)
+    # session = models.ForeignKey()
+    # facilitator = models.ForeignKey()
+
+    def __str__(self):
+        return self.name
