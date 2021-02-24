19227db20407acde991e1a3c7ff4bfca2f06bbf3
Tayef Shah <tayefshah@gmail.com>
2021-02-23 20:51:15 -0500

    added sessions endpoints

--- enrolments/views.py
+++ enrolments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import mixins, permissions, viewsets
 
-# Create your views here.
+from .models import Session
+from .serializers import SessionSerializer
+
+
+class SessionViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
+    http_method_names = [
+        "get",
+    ]
+
+    permission_classes = [permissions.IsAuthenticated]
