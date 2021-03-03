dd8ca053d4e3a8e1d6e92b99cfc966467225df1d
Tayef Shah <tayefshah@gmail.com>
2021-03-03 14:23:28 -0500

    moved post test into method not allowed and renamed session test variables

--- enrolments/tests/views/test_session.py
+++ enrolments/tests/views/test_session.py
@@ -10,8 +10,8 @@ from enrolments.serializers import SessionSerializer
 class SessionTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(email="user@staff.com")
-        self.session_1 = Session.objects.create(season=Session.SPRING, year=2021)
-        self.session_2 = Session.objects.create(season=Session.SUMMER, year=2021)
+        self.session1 = Session.objects.create(season=Session.SPRING, year=2021)
+        self.session2= Session.objects.create(season=Session.SUMMER, year=2021)
 
     def test_get_all_sessions(self):
         url = reverse("sessions-list")
@@ -23,29 +23,26 @@ class SessionTestCase(APITestCase):
         self.assertEqual(
             payload,
             [
-                SessionSerializer(self.session_1).data,
-                SessionSerializer(self.session_2).data,
+                SessionSerializer(self.session1).data,
+                SessionSerializer(self.session2).data,
             ],
         )
 
     def test_get_session(self):
-        url = reverse("sessions-detail", args=[self.session_1.id])
+        url = reverse("sessions-detail", args=[self.session1.id])
         self.client.force_login(self.user)
         response = self.client.get(url)
         payload = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(payload, SessionSerializer(self.session_1).data)
-
-    def test_post_session(self):
-        url = reverse("sessions-list")
-        self.client.force_login(self.user)
-        response = self.client.post(url)
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(payload, SessionSerializer(self.session1).data)
 
     def test_method_not_allowed(self):
-        url = reverse("sessions-detail", args=[self.session_1.id])
+        url = reverse("sessions-detail", args=[self.session1.id])
         self.client.force_login(self.user)
+
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         response = self.client.put(url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
@@ -65,7 +62,7 @@ class SessionTestCase(APITestCase):
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-        url = reverse("sessions-detail", args=[self.session_1.id])
+        url = reverse("sessions-detail", args=[self.session1.id])
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
