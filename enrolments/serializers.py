a8c2e7121baa2b402296968f94021bd6df66bf6c
PRANNOY LAL <prannoylal@gmail.com>
2021-03-02 21:13:56 -0500

    set up serializers and endpoint, tests wip

--- enrolments/serializers.py
+++ enrolments/serializers.py
@@ -1,12 +1,35 @@
 from rest_framework import serializers
-from .models import Session
+from .models import Session, Class
 
 
 class SessionSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Session
         fields = [
             "id",
             "season",
             "year",
+        ]
+
+class ClassListSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Class
+        fields = [
+            "id",
+            "name",
+            "facilitator_id"
+        ]
+
+class SessionDetailsSerializer(serializers.HyperlinkedModelSerializer):
+    classes = ClassListSerializer(many=True)
+
+    class Meta:
+        model = Session
+        fields = [
+            "id",
+            "season",
+            "year",
+            "classes",
         ]
--- enrolments/views.py
+++ enrolments/views.py
@@ -1,7 +1,9 @@
 from rest_framework import mixins, permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Session
-from .serializers import SessionSerializer
+from .serializers import SessionSerializer, SessionDetailsSerializer, ClassListSerializer
 
 
 class SessionViewSet(
@@ -14,5 +16,11 @@ class SessionViewSet(
     http_method_names = [
         "get",
     ]
-
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(methods=['get'], detail=True, permission_classes=[permissions.IsAuthenticated], url_path='classes', url_name='classes')
+    def get_classes(self, request, pk=None):
+        classes = Session.objects.filter(id=pk).first().classes
+        return Response(ClassListSerializer(classes, many=True).data)
+
+
