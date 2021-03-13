b9d5e9d1c2f293ebaad8f9676abc11eeb48076d0
prannoy lal <prannoylal@gmail.com>
2021-03-13 15:44:40 -0500

    Add number of children to FamilySerializer (#32)
    * added serializer method field
    
    * updated serialized and tests

--- registration/tests/test_serializers.py
+++ registration/tests/test_serializers.py
@@ -6,16 +6,30 @@ from registration.serializers import FamilySerializer
 
 class SerializersTestCase(APITestCase):
     def setUp(self):
-        self.parent = Student.objects.create(
-            first_name="Merlin", last_name="Fish", attendee_type="Parent"
+        self.parent1 = Student.objects.create(
+            first_name="Merlin", last_name="Fish", attendee_type=Student.PARENT
         )
+
+        self.parent2 = Student.objects.create(
+            first_name="Gandalf", last_name="Whale", attendee_type=Student.PARENT
+        )
+
         self.family = Family.objects.create(
-            parent=self.parent,
+            parent=self.parent2,
+            email="justkeepswimming@ocean.com",
+            phone_number="123456789",
+            address="1 Django Boulevard",
+            preferred_comms="Shark Tune",
+        )
+
+        self.family_without_children = Family.objects.create(
+            parent=self.parent1,
             email="justkeepswimming@ocean.com",
             phone_number="123456789",
             address="42 Wallaby Way",
             preferred_comms="Whale Song",
         )
+
         self.family_without_parent = Family.objects.create(
             email="justkeepswimming@ocean.com",
             phone_number="123456789",
@@ -23,12 +37,41 @@ class SerializersTestCase(APITestCase):
             preferred_comms="Dolphin Whistle",
         )
 
+        self.child1 = Student.objects.create(
+            first_name="Albus",
+            last_name="Whale",
+            attendee_type=Student.CHILD,
+            family=self.family,
+        )
+
+        self.child2 = Student.objects.create(
+            first_name="Lily",
+            last_name="Whale",
+            attendee_type=Student.CHILD,
+            family=self.family,
+        )
+
+        self.child3 = Student.objects.create(
+            first_name="Harry",
+            last_name="Tuna",
+            attendee_type=Student.CHILD,
+            family=self.family_without_parent,
+        )
+
     def test_family_serializer_parent_name(self):
-        data = FamilySerializer(self.family).data
-        self.assertEqual(data.get("first_name"), self.parent.first_name)
-        self.assertEqual(data.get("last_name"), self.parent.last_name)
+        data = FamilySerializer(self.family_without_children).data
+        self.assertEqual(data.get("first_name"), self.parent1.first_name)
+        self.assertEqual(data.get("last_name"), self.parent1.last_name)
 
     def test_family_serializer_parent_name_none(self):
         data = FamilySerializer(self.family_without_parent).data
         self.assertEqual(data.get("first_name"), "")
         self.assertEqual(data.get("last_name"), "")
+
+    def test_family_serializer_children(self):
+        data = FamilySerializer(self.family).data
+        self.assertEqual(data.get("num_children"), 2)
+
+    def test_family_serializer_children_none(self):
+        data = FamilySerializer(self.family_without_children).data
+        self.assertEqual(data.get("num_children"), 0)
