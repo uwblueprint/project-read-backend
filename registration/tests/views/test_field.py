ed6dde174930ee29368a024fa8c2af4d1fe6bf98
Tayef Shah <tayefshah@gmail.com>
2021-03-20 20:21:51 -0400

    add GET fields endpoint and test cases for related viewset (#36)
    * added GET fields endpoint and views testcases
    
    * ran linter and fixed typos in test cases
    
    * add id field to serializer and remove retrieve and create actions from viewset

---
+++ registration/tests/views/test_field.py
@@ -0,0 +1,68 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from accounts.models import User
+from registration.models import Field
+from registration.serializers import FieldSerializer
+
+
+class FieldTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(email="user@staff.com")
+        self.parent_field = Field.objects.create(
+            role=Field.PARENT,
+            name="email",
+            question="What is your email?",
+            question_type=Field.TEXT,
+            is_default=True,
+            order=1,
+        )
+        self.child_field = Field.objects.create(
+            role=Field.CHILD,
+            name="Date Of Birth",
+            question="When were you born?",
+            question_type=Field.TEXT,
+            is_default=True,
+            order=1,
+        )
+        self.guest_field = Field.objects.create(
+            role=Field.GUEST,
+            name="Relation to Family",
+            question="How are you related?",
+            question_type=Field.TEXT,
+            is_default=True,
+            order=1,
+        )
+
+    def test_get_fields(self):
+        url = reverse("fields-list")
+        self.client.force_authenticate(self.user)
+        response = self.client.get(url)
+        payload = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            payload,
+            [
+                FieldSerializer(self.parent_field).data,
+                FieldSerializer(self.child_field).data,
+                FieldSerializer(self.guest_field).data,
+            ],
+        )
+
+    def test_method_not_allowed(self):
+        url = reverse("fields-list")
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_unauthorized(self):
+        url = reverse("fields-list")
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
