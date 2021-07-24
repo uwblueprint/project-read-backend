76420c9b6aac1571abdf773594eaf1a9525a76f5
Lambert Liu <lambert0928@outlook.com>
2021-07-24 12:58:24 -0400

    fix accidental delete

--- enrolments/tests/test_serializers.py
+++ enrolments/tests/test_serializers.py
@@ -187,15 +187,26 @@ class EnrolmentSerializerTestCase(TestCase):
             year=2021,
             start_date=date(2021, 5, 15),
         )
-        self.preferred_class = Class.objects.create(
-            name="Preferred Class",
+        self.other_session = Session.objects.create(
+            season="Fall",
+            year=2021,
+            start_date=date(2021, 5, 15),
+        )
+        self.class1 = Class.objects.create(
+            name="Tuesday & Saturday",
             session_id=self.session.id,
             facilitator_id=None,
             attendance=[],
         )
-        self.enrolled_class = Class.objects.create(
-            name="Enrolled Class",
+        self.class2 = Class.objects.create(
+            name="Wednesday & Thursday",
             session_id=self.session.id,
+            facilitator_id=None,
+            attendance=[],
+        )
+        self.class_not_in_session = Class.objects.create(
+            name="Wednesday & Thursday",
+            session_id=self.other_session.id,
             facilitator_id=None,
             attendance=[],
         )
@@ -203,10 +214,17 @@ class EnrolmentSerializerTestCase(TestCase):
             active=False,
             family=self.family,
             session=self.session,
-            preferred_class=self.preferred_class,
-            enrolled_class=self.enrolled_class,
+            preferred_class=self.class1,
+            enrolled_class=self.class2,
             status=Enrolment.REGISTERED,
         )
+        self.update_request = {
+            "id": self.enrolment.id,
+            "session": self.session.id,
+            "preferred_class": self.class2.id,
+            "enrolled_class": self.class1.id,
+            "status": Enrolment.CLASS_ALLOCATED,
+        }
 
     def test_enrolment_serializer(self):
         self.assertEqual(
@@ -218,17 +236,35 @@ class EnrolmentSerializerTestCase(TestCase):
                     "year": self.session.year,
                 },
                 "preferred_class": {
-                    "id": self.preferred_class.id,
-                    "name": self.preferred_class.name,
+                    "id": self.class1.id,
+                    "name": self.class1.name,
                 },
                 "enrolled_class": {
-                    "id": self.enrolled_class.id,
-                    "name": self.enrolled_class.name,
+                    "id": self.class2.id,
+                    "name": self.class2.name,
                 },
                 "status": self.enrolment.status,
             },
             EnrolmentSerializer(self.enrolment).data,
         )
+
+    def test_enrolment_update(self):
+        serializer = EnrolmentSerializer(self.enrolment, data=self.update_request)
+        self.assertTrue(serializer.is_valid())
+
+    def test_enrolment_update__read_only_session(self):
+        data = dict(self.update_request)
+        data["session"] = self.other_session.id
+        serializer = EnrolmentSerializer(self.enrolment, data=data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+        self.assertEqual(self.enrolment.session, self.session)
+
+    def test_enrolment_update__invalid_class(self):
+        data = dict(self.update_request)
+        data["preferred_class"] = self.class_not_in_session.id
+        serializer = EnrolmentSerializer(self.enrolment, data=data)
+        self.assertFalse(serializer.is_valid())
 
 
 class EnrolmentCreateSerializerTestCase(TestCase):
