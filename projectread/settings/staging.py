2b3937f4c980bd0b2cd78b8698f989c16751f8cf
Tayef Shah <tayefshah@gmail.com>
2021-07-31 15:03:36 -0400

    add scheme to staging origins

--- projectread/settings/staging.py
+++ projectread/settings/staging.py
@@ -2,6 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = ["project-read-backend.herokuapp.com"]
 CORS_ALLOWED_ORIGINS = [
-    "project-read-frontend.herokuapp.com",
+    "https://project-read-frontend.herokuapp.com",
     "https://getsetlearn.netlify.app",
 ]
