fcb905bdac13bbdea06bc3c8bf570755c1419f42
jolenezheng <jolenezheng18@gmail.com>
2021-08-23 19:46:46 -0400

    fixed session create tests, updated fields error msg

--- enrolments/tests/serializers/test_session_create.py
+++ enrolments/tests/serializers/test_session_create.py
@@ -4,34 +4,52 @@ from unittest.mock import patch
 
 from accounts.models import User
 from enrolments.models import Class, Session
+from registration.models import Field
 from enrolments.serializers import SessionCreateSerializer
 from datetime import date
 
 
 class SessionCreateSerializerTestCase(TestCase):
     def setUp(self):
+        Field.objects.bulk_create(
+            [
+                Field(
+                    role=Field.PARENT,
+                    name="Drip or drown?",
+                    question="Do you have drip?",
+                    question_type=Field.TEXT,
+                    is_default=False,
+                    order=1,
+                ),
+                Field(
+                    role=Field.CHILD,
+                    name="Swag or square?",
+                    question="Do you have swag?",
+                    question_type=Field.TEXT,
+                    is_default=True,
+                    order=2,
+                ),
+                Field(
+                    role=Field.GUEST,
+                    name="Ice or ill?",
+                    question="Do you have ice?",
+                    question_type=Field.TEXT,
+                    is_default=False,
+                    order=3,
+                ),
+            ]
+        )
+        self.field_ids = list(Field.objects.values_list("id", flat=True))
+
         self.name = "Fall 2020"
         self.start_date = date(2020, 9, 5)
         self.fields = [1, 2, 3]
         self.facilitator = User.objects.create(email="user@staff.com")
 
-        # self.class1 = Class.objects.create(
-        #     name="Test Class 1",
-        #     days=[Class.TUESDAY, Class.SATURDAY],
-        #     location="219 Waterloo Way",
-        #     facilitator=self.facilitator.id,
-        # )
-        # self.class2 = Class.objects.create(
-        #     name="Test Class 2",
-        #     days=[Class.MONDAY, Class.THURSDAY],
-        #     location="1378 Kitchener Street",
-        #     facilitator=self.facilitator.id,
-        # )
         self.session_payload = {
             "name": self.name,
             "start_date": self.start_date,
-            "fields": self.fields,
-            # "classes": [self.class1, self.class2],
+            "fields": self.field_ids,
             "classes": [
                 {
                     "name": "Test Class 1",
@@ -48,7 +66,6 @@ class SessionCreateSerializerTestCase(TestCase):
             ],
         }
 
-    # @patch("enrolments.serializers.SessionCreateSerializer.create")
     def test_family_detail_serializer_create(self):
         serializer = SessionCreateSerializer(data=self.session_payload)
         self.assertTrue(serializer.is_valid())
