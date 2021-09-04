5e0221550a8c828e441b988b3822a92081519c9b
jolenezheng <jolenezheng18@gmail.com>
2021-09-04 19:10:15 -0400

    removed comments

--- enrolments/tests/serializers/test_class_create.py
+++ enrolments/tests/serializers/test_class_create.py
@@ -11,50 +11,9 @@ from enrolments.serializers import ClassCreateSerializer
 
 class ClassCreateSerializerTestCase(TestCase):
     def setUp(self):
-
-        # Field.objects.bulk_create(
-        #     [
-        #         Field(
-        #             role=Field.PARENT,
-        #             name="Drip or drown?",
-        #             question="Do you have drip?",
-        #             question_type=Field.TEXT,
-        #             is_default=False,
-        #             order=1,
-        #         ),
-        #         Field(
-        #             role=Field.CHILD,
-        #             name="Swag or square?",
-        #             question="Do you have swag?",
-        #             question_type=Field.TEXT,
-        #             is_default=True,
-        #             order=2,
-        #         ),
-        #         Field(
-        #             role=Field.GUEST,
-        #             name="Ice or ill?",
-        #             question="Do you have ice?",
-        #             question_type=Field.TEXT,
-        #             is_default=False,
-        #             order=3,
-        #         ),
-        #     ]
-        # )
-
-        # self.session_name = "Fall 2020"
-        # self.start_date = date(2020, 9, 5)
-        # self.field_ids = list(Field.objects.values_list("id", flat=True))
         self.session = Session.objects.create(
             name="Summer 2021", start_date=date(2021, 1, 1)
         )
-        # self.session = Session.objects.create(name="Fall 2020", start_date=self.start_date, fields=self.field_ids, classes=[])
-        # self.session = {
-        #     "name": self.session_name,
-        #     "start_date": self.start_date,
-        #     "fields": self.field_ids,
-        #     "classes": [],
-        # }
-
         self.name = "Class 1"
         self.days = [Class.MONDAY, Class.THURSDAY]
         self.location = "Waterloo"
